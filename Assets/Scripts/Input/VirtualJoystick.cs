66d96d8be8cd2b290a358a9c679d206a7dd0e904
krokoloko <daan.j.ruting@gmail.com>
2020-03-09 18:05:19 +0100

    Finished VirtualJoystick input
    You can now add events on the VirtualJoystick script or call either functions that returns the travel or direction of the joystick. It's also fail safe of other touch inputs that can appear.

--- Assets/Scripts/Input/VirtualJoystick.cs
+++ Assets/Scripts/Input/VirtualJoystick.cs
@@ -1,17 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
+
+public class JoystickData : EventArgs
+{
+    public Vector2 direction;
+    public float distance;
+}
+[System.Serializable]
+public class JoystickEvent : UnityEvent<JoystickData> { }
+
 public class VirtualJoystick : MonoBehaviour
 {
     [SerializeField]
-    private float _maxOffset;
+    private double _maxOffset;
 
     private bool _joystickDetected = false;
-
-    private Vector2 _pointA;
-    private Vector2 _pointB;
+    private int _touchIndex = -1;
+    private int _touchTracker;
+    private Vector2 _centerPosition;
+    private Vector2 _touchPosition;
 
     [SerializeField]
     private Transform _innerCircle;
@@ -19,45 +30,99 @@ public class VirtualJoystick : MonoBehaviour
     private Transform _outerCircle;
 
     [SerializeField]
-    private UnityEvent OnJoystickEnter;
+    private JoystickEvent _onJoystickEnter;
     [SerializeField]
-    private UnityEvent OnJoystickStay;
+    private JoystickEvent _onJoystickStay;
     [SerializeField]
-    private UnityEvent OnJoystickExit;
+    private JoystickEvent _onJoystickExit;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _touchPosition = _outerCircle.GetComponent<RectTransform>().position;
+        _centerPosition = _outerCircle.GetComponent<RectTransform>().position;
+        Debug.Log(_centerPosition);
     }
 
-    public void IsPressed()
+    //Gets the Vector2 of the joystick position in a normalised value comparing with the centerposition
+    public Vector2 GetVector2()
     {
-           
+        return (_centerPosition - _touchPosition).normalized;
+    }
+
+    //Gets the distance between the touch position and center position of the joystick
+    private float GetDistance()
+    {
+        return Vector2.Distance(_centerPosition,_touchPosition);
+    }
+
+    //Gets the normalised travel between the center of the joystick and the innerCircle/joystick itself
+    public float GetNormalisedDistance()
+    {
+        return Mathf.Min(Vector2.Distance(_centerPosition, _innerCircle.position)/(float)_maxOffset,1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(_joystickDetected);
-        if (Input.touchCount > 0)
+        if (_touchIndex >= 0)
         {
-            Debug.Log("touch detected");
-            Touch touch = Input.GetTouch(0);
+            JoystickData data = new JoystickData();
 
-            if (_outerCircle.GetComponent<RectTransform>().)
+            Touch touch = Input.GetTouch(_touchIndex);
+            
+            _touchPosition = touch.position;
+
+            float distance = GetDistance();
+
+            //The conditions to trigger OnJoystickEnter is if the touch position is in range of the joystick and if it hasn't been touched
+            if (distance <= _maxOffset && !_joystickDetected)
             {
+                _joystickDetected = true;
+
+                //Activate OnJoystickEnter event unless it has no subscribed actions
+                data.direction = GetVector2();
+                data.distance = GetNormalisedDistance();
+                _onJoystickEnter?.Invoke(data);
+            }
+            if (_joystickDetected)
+            {
+                //If the touch isn't active, then the position of the innerCircle/joystick will be centered
                 if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
                 {
                     _innerCircle.position = _outerCircle.position;
-                    OnJoystickExit?.Invoke();
+                    _joystickDetected = false;
+                    _touchPosition = _centerPosition;
+                    
+                    //Activate OnJoystickExit event unless it has no subscribed actions
+                    data.direction = GetVector2();
+                    data.distance = GetNormalisedDistance();
+                    _onJoystickExit?.Invoke(data);
                 }
                 else
                 {
-                    _innerCircle.position = touch.position;
+                    //This calculation clamps the innerCircle/joystick inside the maxOffset
+                    Vector2 newPosition = (_touchPosition - _centerPosition).normalized*Mathf.Clamp(distance,0,(float)_maxOffset)+_centerPosition;
+
+                    //Updates position of the innerCircle/joystick
+                    data.direction = GetVector2();
+                    data.distance = GetNormalisedDistance();
+
+                    //Activate OnJoystickStay event unless it has no subscribed actions
+                    _onJoystickStay?.Invoke(data);
+
+                    //Updates position of the innerCircle/Joystick
+                    _innerCircle.position = newPosition;
                 }
-            }            
+            }
         }
-        _joystickDetected = false;
+        if (Input.touchCount != _touchTracker && !_joystickDetected)
+        {
+            _touchTracker = Input.touchCount;
+            _touchIndex = _touchTracker - 1;
+            return;
+        }
     }
 }
