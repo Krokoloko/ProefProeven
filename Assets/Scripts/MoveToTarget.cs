9470b605d41ca104532a9aa49774f3f652acb109
Lucas Muileboom <interns@ar-producties.nl>
2020-03-09 12:23:31 +0100

    Added NavMesh PathFinding

---
+++ Assets/Scripts/MoveToTarget.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToTarget : MonoBehaviour 
+{
+    private NavMeshAgent navMeshAgent;
+    private bool canMove = true;
+
+    [SerializeField]private GameObject Test;
+
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+    private void Update() 
+    {
+        if(canMove) 
+        {
+            MoveToPoint(Test.transform.position);
+        }
+    }
+    private void MoveToPoint(Vector3 TargetPosition) 
+    {
+        navMeshAgent.SetDestination(TargetPosition);
+    }
+    
+}
