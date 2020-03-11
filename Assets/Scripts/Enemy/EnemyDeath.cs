a691fe0c3a4cbcf11c3cd11e0ba675a1d53e1665
Lucas Muileboom <interns@ar-producties.nl>
2020-03-11 15:21:34 +0100

    Added EnemyDeath script

---
+++ Assets/Scripts/Enemy/EnemyDeath.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDeath : MonoBehaviour
+{
+    private GenericHealth genericHealth;
+
+    private void Start() 
+    {
+        genericHealth = GetComponent<GenericHealth>();
+        genericHealth.onDeath.AddListener(EnemyDied);
+    }
+    private void EnemyDied() 
+    {
+        Destroy(this.gameObject);
+        print("enemy died");
+    }
+    private void OnDestroy() 
+    {
+        genericHealth.onDeath.RemoveListener(EnemyDied);
+    }
+}
