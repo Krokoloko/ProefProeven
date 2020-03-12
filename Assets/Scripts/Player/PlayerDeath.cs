9b1ada79b639b16c7ed06c816404914373974836
Lucas Muileboom <interns@ar-producties.nl>
2020-03-12 11:48:33 +0100

    Added PlayerDeath scipt

---
+++ Assets/Scripts/Player/PlayerDeath.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    private SwitchScene _switchScene;
+    private GenericHealth _genericHealth;    
+
+    private void Start() 
+    {
+        _switchScene = GetComponent<SwitchScene>();
+        _genericHealth = GetComponent<GenericHealth>();        
+        _genericHealth.onDeath.AddListener(PlayerDied);
+    }
+    private void PlayerDied() 
+    {
+        _switchScene.SwitchScenes();
+        print("PlayerDied");
+    }
+    private void OnDestroy() 
+    {
+        _genericHealth.onDeath.RemoveListener(PlayerDied);
+    }
+}
