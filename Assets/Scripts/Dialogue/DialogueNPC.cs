2b3e36d4db9cad891a722a098be4363912341755
Koen Sparreboom <koensparreboom@gmail.com>
2020-03-12 11:55:56 +0100

    Added dialogue system

---
+++ Assets/Scripts/Dialogue/DialogueNPC.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class DialogueNPC : MonoBehaviour
+{
+    [Serializable]
+    public class DialogueData
+    {
+        public string name;
+        public Sprite sprite;
+        public string[] dialogueLines;
+    }
+
+    [SerializeField]
+    private DialogueData[] _dialogueData;
+    
+    // Placeholder way of activating dialogue
+    [SerializeField]
+    private KeyCode _keyCode;
+    
+    private void Update()
+    {
+        // More placeholder to activate the dialogue
+        if (Input.GetKeyDown(_keyCode))
+        {
+            FindObjectOfType<DialogueBox>().StartDialogue(_dialogueData);
+        }
+    }
+}
