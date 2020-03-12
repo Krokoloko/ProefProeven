1471ebc321de9cd8e6caef524b4f744a2e4bc653
Lucas Muileboom <interns@ar-producties.nl>
2020-03-12 10:45:45 +0100

    Added SceneSwitch scipt & end of level colider

---
+++ Assets/Scripts/SceneSwitch/LevelEnd.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEnd : MonoBehaviour
+{
+    private SwitchScene _switchScene;
+
+    private void Start()
+    {
+        _switchScene = GetComponent<SwitchScene>();
+    }
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            _switchScene.SwitchScenes();
+        }
+    }
+}
